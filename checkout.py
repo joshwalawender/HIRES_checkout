0bd1cceac43a1d75a791205cf0a23e7917a07e08
Josh Walawender <jmwalawender@gmail.com>
2016-12-20 17:06:28 -1000

    Update dark current estimate to use ccdproc.CCDData objects instead of HIRESimage

--- checkout.py
+++ checkout.py
@@ -11,9 +11,10 @@ from glob import glob
 
 import numpy as np
 from astropy import units as u
-# from astropy.table import Table, Column
+from astropy.table import Table, Column
 from astropy.modeling import models, fitting
 from astropy import stats
+from astropy.io import fits
 
 import ccdproc
 
@@ -49,11 +50,16 @@ def main():
     parser.add_argument("--fake",
         action="store_true", dest="fake",
         default=True, help="Use test data, do not take new images.")
+    parser.add_argument("--noplots",
+        action="store_true", dest="noplots",
+        default=False, help="Do not make plots.")
     ## add arguments
 #     parser.add_argument("--input",
 #         type=str, dest="input",
 #         help="The input.")
     args = parser.parse_args()
+
+    plots = not args.noplots
 
     ##-------------------------------------------------------------------------
     ## Create logger object
@@ -94,8 +100,10 @@ def main():
     ## Determine Bias Stats for each Chip
     clipping_sigma = 5
     clipping_iters = 1
-    plt.figure(figsize=(9,15), dpi=72)
-    binsize = 1.0
+    if plots:
+        plt.figure(figsize=(9,15), dpi=72)
+        binsize = 1.0
+    master_biases = {}
     for chip in [1,2,3]:
         logger.info('Analyzing Chip {:d}'.format(chip))
         ax = plt.subplot(3,1,chip)
@@ -113,6 +121,7 @@ def main():
                 biases.append(ccdproc.fits_ccddata_reader(bias_file, unit='adu', hdu=chip))
         logger.debug('  Making master bias')
         master_bias = ccdproc.combine(biases, combine='median')
+        master_biases[chip] = master_bias
         mean, median, stddev = stats.sigma_clipped_stats(master_bias.data,
                                      sigma=clipping_sigma,
                                      iters=clipping_iters) * u.adu
@@ -131,85 +140,84 @@ def main():
         ##---------------------------------------------------------------------
         ## Plot Bias Frame Histograms
         ##---------------------------------------------------------------------
-        logger.debug('  Generating histogram of bias difference image values')
-        bmin = np.floor(min(diff.data.ravel())/binsize)*binsize - binsize/2.
-        bmax = np.ceil(max(diff.data.ravel())/binsize)*binsize + binsize/2.
-        bins = np.arange(bmin,bmax,binsize)
-        hist, bins = np.histogram(diff.data.ravel(), bins=bins)
-        centers = (bins[:-1] + bins[1:]) / 2
-        gaussian = models.Gaussian1D(amplitude=max(hist),\
-                                     mean=mean,\
-                                     stddev=RN)
-        gaussian_plot = [gaussian(x) for x in centers]
-        plt.bar(centers, hist,\
-                align='center', width=0.7*binsize, log=True, color='b', alpha=0.5,\
-                label='Blue CCD Pixel Count Histogram')
-        plt.plot(centers, gaussian_plot, 'b-', alpha=0.8,\
-                 label='Gaussian with sigma = {:.2f}'.format(RN))
-        plt.plot([mean.value, mean.value], [1, 2*max(hist)], label='Mean Pixel Value')
-        plt.xlim(np.floor(mean.value-7.*RN.value), np.ceil(mean.value+7.*RN.value))
-        plt.ylim(1, 2*max(hist))
-        ax.set_xlabel('Counts (ADU)', fontsize=10)
-        ax.set_ylabel('Number of Pixels', fontsize=10)
-        ax.grid()
-        ax.legend(loc='best', fontsize=10)
+        if plots:
+            logger.debug('  Generating histogram of bias difference image values')
+            bmin = np.floor(min(diff.data.ravel())/binsize)*binsize - binsize/2.
+            bmax = np.ceil(max(diff.data.ravel())/binsize)*binsize + binsize/2.
+            bins = np.arange(bmin,bmax,binsize)
+            hist, bins = np.histogram(diff.data.ravel(), bins=bins)
+            centers = (bins[:-1] + bins[1:]) / 2
+            gaussian = models.Gaussian1D(amplitude=max(hist),\
+                                         mean=mean,\
+                                         stddev=RN)
+            gaussian_plot = [gaussian(x) for x in centers]
+            plt.bar(centers, hist,\
+                    align='center', width=0.7*binsize, log=True, color='b', alpha=0.5,\
+                    label='Blue CCD Pixel Count Histogram')
+            plt.plot(centers, gaussian_plot, 'b-', alpha=0.8,\
+                     label='Gaussian with sigma = {:.2f}'.format(RN))
+            plt.plot([mean.value, mean.value], [1, 2*max(hist)], label='Mean Pixel Value')
+            plt.xlim(np.floor(mean.value-7.*RN.value), np.ceil(mean.value+7.*RN.value))
+            plt.ylim(1, 2*max(hist))
+            ax.set_xlabel('Counts (ADU)', fontsize=10)
+            ax.set_ylabel('Number of Pixels', fontsize=10)
+            ax.grid()
+            ax.legend(loc='best', fontsize=10)
 
-    plotfilename = 'BiasHistogram.png'
-    logger.info('Saving: {}'.format(plotfilename))
-    plt.savefig(plotfilename, dpi=72, bbox_inches='tight')
-    plt.close()
-    logger.info('Done.')
+    if plots:
+        plotfilename = 'BiasHistogram.png'
+        logger.info('Saving: {}'.format(plotfilename))
+        plt.savefig(plotfilename, dpi=72, bbox_inches='tight')
+        plt.close()
 
+
+    ##-------------------------------------------------------------------------
+    ## Determine Dark Current
+    ##-------------------------------------------------------------------------
+    if args.fake:
+        dark_files = glob('/home/jwalawender/HIRES_Test_Data/*.fits')[10:]
+    else:
+        pass
+
+    ndarks = len(dark_files)
+    ## Determine Dark Stats for each Chip
+    clipping_sigma = 5
+    clipping_iters = 1
+    plt.figure(figsize=(9,15), dpi=72)
+    binsize = 1.0
+
+    dark_table = Table(names=('filename', 'exptime', 'chip', 'mean', 'median', 'stddev'),\
+                       dtype=('a64', 'f4', 'i4', 'f4', 'f4', 'f4'))
+    logger.info('Analyzing bias subtracted dark frames to measure dark current.')
+    logger.info('  Determining image statistics using sigma clipping algorithm.')
+    logger.info('    sigma={:d}, iters={:d}'.format(clipping_sigma, clipping_iters))
+
+    for dark_file in dark_files:
+        hdr = fits.getheader(dark_file, 0)
+        exptime = float(hdr['EXPTIME'])
+        for chip in [1,2,3]:
+            dark = ccdproc.fits_ccddata_reader(dark_file, unit='adu', hdu=chip)
+            dark_diff = ccdproc.subtract_bias(dark, master_biases[chip])
+            mean, median, stddev = stats.sigma_clipped_stats(dark_diff.data,
+                                         sigma=clipping_sigma,
+                                         iters=clipping_iters) * u.adu
+            dark_table.add_row([dark_file, exptime, chip, mean, median, stddev])
+
+    logger.info('  Fitting line to levels as a function of exposure time')
+    line = models.Linear1D(intercept=0, slope=0)
+    line.intercept.fixed = True
+    fitter = fitting.LinearLSQFitter()
+    for chip in [1,2,3]:
+        dc_fit = fitter(line, dark_table[dark_table['chip'] == chip]['exptime'],\
+                              dark_table[dark_table['chip'] == chip]['mean'])
+        logger.info('Dark Current (ext={:d}) = {:.2f} ADU/600s'.format(chip,
+                    dc_fit.slope.value*600.))
 
 
     sys.exit(0)
 
 
 
-    ##-------------------------------------------------------------------------
-    ## Determine Dark Current
-    ##-------------------------------------------------------------------------
-    ndarks = 5
-    exptimes=[1, 60, 600]
-    clipping_sigma = 5
-    clipping_iters = 3
-    dark_files = []
-    for exptime in exptimes:
-        new_files = hires.take_darks(n=ndarks, exptimes=exptimes)
-        dark_files.extend(new_files)
-    dark_table = Table(names=('filename', 'exptime', 'chip', 'mean', 'median', 'stddev'),\
-                       dtype=('a64', 'f4', 'a4', 'f4', 'f4', 'f4'))
-    logger.info('Analyzing bias subtracted dark frames to measure dark current.')
-    logger.info('  Determining image statistics using sigma clipping algorithm.')
-    logger.info('    sigma={:d}, iters={:d}'.format(clipping_sigma, clipping_iters))
-    for dark_file in dark_files:
-        dark = HIRESimage(fits_file=dark_file, logger=logger)
-        exptime = float(dark.header['EXPTIME'])
-        dark_diff = dark.subtract_bias(master_bias)
-        dark_diff.calculate_stats(sigma=clipping_sigma, iters=clipping_iters)
-        dark_table.add_row([dark_file, exptime, 'B', dark_diff.b_mean,\
-                            dark_diff.b_median, dark_diff.b_stddev])
-        dark_table.add_row([dark_file, exptime, 'G', dark_diff.g_mean,\
-                            dark_diff.g_median, dark_diff.g_stddev])
-        dark_table.add_row([dark_file, exptime, 'R', dark_diff.r_mean,\
-                            dark_diff.r_median, dark_diff.r_stddev])
-    dark_table.write('dark_table.txt', format='ascii.csv')
-
-    dark_table = Table.read('dark_table.txt', format='ascii.csv')
-
-    logger.info('  Fitting line to levels as a function of exposure time')
-    line = models.Linear1D(intercept=0, slope=0)
-    line.intercept.fixed = True
-    fitter = fitting.LinearLSQFitter()
-    dc_fit_B = fitter(line, dark_table[dark_table['chip'] == 'B']['exptime'],\
-                            dark_table[dark_table['chip'] == 'B']['mean'])
-    dc_fit_G = fitter(line, dark_table[dark_table['chip'] == 'G']['exptime'],\
-                            dark_table[dark_table['chip'] == 'G']['mean'])
-    dc_fit_R = fitter(line, dark_table[dark_table['chip'] == 'R']['exptime'],\
-                            dark_table[dark_table['chip'] == 'R']['mean'])
-    logger.info('Dark Current (blue) = {:.2f} ADU/600s'.format(dc_fit_B.slope.value*600.))
-    logger.info('Dark Current (green) = {:.2f} ADU/600s'.format(dc_fit_G.slope.value*600.))
-    logger.info('Dark Current (red) = {:.2f} ADU/600s'.format(dc_fit_R.slope.value*600.))
 
 
     ##-------------------------------------------------------------------------
