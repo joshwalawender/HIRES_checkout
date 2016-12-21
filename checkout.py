cd981ba4a9ecf43757aeacaf6d9dd3ff2616886c
Josh Walawender <jmwalawender@gmail.com>
2016-12-20 15:02:12 -1000

    Start replacing HIRESimage structure with simples ccdproc.CCDData based structure

--- checkout.py
+++ checkout.py
@@ -7,24 +7,29 @@ import sys
 import os
 import argparse
 import logging
+from glob import glob
 
 import numpy as np
 from astropy import units as u
-from astropy.table import Table, Column
+# from astropy.table import Table, Column
 from astropy.modeling import models, fitting
+from astropy import stats
 
-import matplotlib
-matplotlib.use('Agg')
-matplotlib.rcParams['font.size'] = 10
-import matplotlib.pyplot as plt
+import ccdproc
 
 ## Suppress astropy log
 from astropy import log
 log.setLevel('ERROR')
 # log.disable_warnings_logging()
 
-from instruments import HIRES
-from images import HIRESimage, HIREScombine
+import matplotlib
+matplotlib.use('Agg')
+matplotlib.rcParams['font.size'] = 10
+import matplotlib.pyplot as plt
+
+# from instruments import HIRES
+
+
 
 ##-------------------------------------------------------------------------
 ## Main Program
@@ -43,7 +48,7 @@ def main():
         default=False, help="Be verbose! (default = False)")
     parser.add_argument("--fake",
         action="store_true", dest="fake",
-        default=False, help="Use test data, do not take new images.")
+        default=True, help="Use test data, do not take new images.")
     ## add arguments
 #     parser.add_argument("--input",
 #         type=str, dest="input",
@@ -61,7 +66,8 @@ def main():
         LogConsoleHandler.setLevel(logging.DEBUG)
     else:
         LogConsoleHandler.setLevel(logging.INFO)
-    LogFormat = logging.Formatter('%(asctime)23s %(levelname)8s: %(message)s')
+    LogFormat = logging.Formatter('%(asctime)s %(levelname)8s: %(message)s',
+                                  datefmt='%Y%m%d %H:%M:%S')
     LogConsoleHandler.setFormatter(LogFormat)
     logger.addHandler(LogConsoleHandler)
     ## Set up file output
@@ -71,159 +77,94 @@ def main():
 #     LogFileHandler.setFormatter(LogFormat)
 #     logger.addHandler(LogFileHandler)
 
-    hires = HIRES(logger=logger)
+#     hires = HIRES(logger=logger)
 
     ##-------------------------------------------------------------------------
     ## Determine Read Noise
     ##-------------------------------------------------------------------------
+    if args.fake:
+        bias_files = glob('/home/jwalawender/HIRES_Test_Data/*.fits')[0:10]
+    else:
+        pass
+#         nbiases = 3
+#         bias_files = hires.take_bias(n=nbiases)
+
+    nbiases = len(bias_files)
+
+    ## Determine Bias Stats for each Chip
     clipping_sigma = 5
     clipping_iters = 1
-    nbiases = 3
-    bias_files = hires.take_bias(n=nbiases)
-    nbiases = len(bias_files)
-    ## First bias frame is the reference
-    bias = HIRESimage(fits_file=bias_files[0], logger=logger)
-    bias.calculate_stats(sigma=clipping_sigma, iters=clipping_iters)
-    logger.debug('  Bias B (mean, median, stddev) = {:.2f}, {:.2f}, {:.2f}'.format(\
-                 bias.b_mean, bias.b_median, bias.b_stddev))
-    logger.debug('  Bias G (mean, median, stddev) = {:.2f}, {:.2f}, {:.2f}'.format(\
-                 bias.g_mean, bias.g_median, bias.g_stddev))
-    logger.debug('  Bias R (mean, median, stddev) = {:.2f}, {:.2f}, {:.2f}'.format(\
-                 bias.r_mean, bias.r_median, bias.r_stddev))
-    
-    ## Create master bias from next n-1 frames
-    logger.info('Reading bias frames for creating master bias')
-    image_list = [HIRESimage(fits_file=bias_file, logger=logger)
-                  for bias_file in bias_files if bias_file != bias_files[0]]
-    logger.info('Combining {} files to make master bias'.format(len(image_list)))
-    master_bias = HIREScombine(image_list, combine='median', logger=logger)
-    master_bias.calculate_stats(sigma=clipping_sigma, iters=clipping_iters)
-    logger.debug('  Master Bias B (mean, median, stddev) = {:.2f}, {:.2f}, {:.2f}'.format(\
-                 master_bias.b_mean, master_bias.b_median, master_bias.b_stddev))
-    logger.debug('  Master Bias G (mean, median, stddev) = {:.2f}, {:.2f}, {:.2f}'.format(\
-                 master_bias.g_mean, master_bias.g_median, master_bias.g_stddev))
-    logger.debug('  Master Bias R (mean, median, stddev) = {:.2f}, {:.2f}, {:.2f}'.format(\
-                 master_bias.r_mean, master_bias.r_median, master_bias.r_stddev))
-
-    ## Create difference between a single bias frame and the master bias
-    logger.info('Creating bias difference frame')
-    bias_diff = bias.subtract_bias(master_bias)
-    logger.info('  Determining image statistics using sigma clipping algorithm.')
-    logger.info('    sigma={:d}, iters={:d}'.format(clipping_sigma, clipping_iters))
-    bias_diff.calculate_stats(sigma=clipping_sigma, iters=clipping_iters)
-    logger.debug('  Bias Difference B (mean, median, stddev) = {:.2f}, {:.2f}, {:.2f}'.format(\
-                 bias_diff.b_mean, bias_diff.b_median, bias_diff.b_stddev))
-    logger.debug('  Bias Difference G (mean, median, stddev) = {:.2f}, {:.2f}, {:.2f}'.format(\
-                 bias_diff.g_mean, bias_diff.g_median, bias_diff.g_stddev))
-    logger.debug('  Bias Difference R (mean, median, stddev) = {:.2f}, {:.2f}, {:.2f}'.format(\
-                 bias_diff.r_mean, bias_diff.r_median, bias_diff.r_stddev))
-
-    ## Calculate read noise from the difference frame's stddev
-    RN_b = bias_diff.b_stddev / np.sqrt(1.+1./(nbiases-1))
-    RN_g = bias_diff.g_stddev / np.sqrt(1.+1./(nbiases-1))
-    RN_r = bias_diff.r_stddev / np.sqrt(1.+1./(nbiases-1))
-    logger.info('Read Noise (blue) = {:.2f} ADU'.format(RN_b))
-    logger.info('Read Noise (green) = {:.2f} ADU'.format(RN_g))
-    logger.info('Read Noise (red) = {:.2f} ADU'.format(RN_r))
-
-
-    ##-------------------------------------------------------------------------
-    ## Plot Bias Frame Histograms
-    ##-------------------------------------------------------------------------
-    logger.info('Generating figure with pixel value histograms')
-    plt.figure()
-    ax = plt.subplot(311)
+    plt.figure(figsize=(9,15), dpi=72)
     binsize = 1.0
-    bmin = np.floor(min(bias_diff.b.data.ravel())/binsize)*binsize - binsize/2.
-    bmax = np.ceil(max(bias_diff.b.data.ravel())/binsize)*binsize + binsize/2.
-    bins = np.arange(bmin,bmax,binsize)
-    hist, bins = np.histogram(bias_diff.b.data.ravel(), bins=bins)
-    centers = (bins[:-1] + bins[1:]) / 2
+    for chip in [1,2,3]:
+        logger.info('Analyzing Chip {:d}'.format(chip))
+        ax = plt.subplot(3,1,chip)
+        biases = []
+        for bias_file in bias_files:
+            logger.debug('  Reading {}'.format(bias_file))
+            if bias_file == bias_files[0]:
+                bias = ccdproc.fits_ccddata_reader(bias_file, unit='adu', hdu=chip)
+                mean, median, stddev = stats.sigma_clipped_stats(bias.data,
+                                             sigma=clipping_sigma,
+                                             iters=clipping_iters) * u.adu
+                logger.debug('  Bias ext={:d} (mean, median, stddev) = {:.2f}, {:.2f}, {:.2f}'.format(\
+                             chip, mean, median, stddev))
+            else:
+                biases.append(ccdproc.fits_ccddata_reader(bias_file, unit='adu', hdu=chip))
+        logger.debug('  Making master bias')
+        master_bias = ccdproc.combine(biases, combine='median')
+        mean, median, stddev = stats.sigma_clipped_stats(master_bias.data,
+                                     sigma=clipping_sigma,
+                                     iters=clipping_iters) * u.adu
+        logger.debug('  Master Bias ext={:d} (mean, median, stddev) = {:.2f}, {:.2f}, {:.2f}'.format(\
+                     chip, mean, median, stddev))
 
-    gaussian = models.Gaussian1D(amplitude=max(hist),\
-                                 mean=bias_diff.b_mean,\
-                                 stddev=RN_b)
-    gaussian_plot = [gaussian(x) for x in centers]
-    plt.bar(centers, hist,\
-            align='center', width=0.7*binsize, log=True, color='b', alpha=0.5,\
-            label='Blue CCD Pixel Count Histogram')
-    plt.plot(centers, gaussian_plot, 'b-', alpha=0.8,\
-             label='Gaussian with sigma = {:.2f} ADU'.format(RN_b))
-    plt.plot([bias_diff.b_mean, bias_diff.b_mean], [1, 2*max(hist)],\
-             label='Mean Pixel Value')
-    plt.xlim(np.floor(bias_diff.b_mean-7.*RN_b),\
-             np.ceil(bias_diff.b_mean+7.*RN_b))
-    plt.ylim(10, 2*max(hist))
-    ax.set_xlabel('Counts (ADU)', fontsize=10)
-    ax.set_ylabel('Number of Pixels', fontsize=10)
-    ax.grid()
-    ax.legend(loc='best', fontsize=10)
-    ax.grid()
+        diff = bias.subtract(master_bias)
+        mean, median, stddev = stats.sigma_clipped_stats(diff.data,
+                                     sigma=clipping_sigma,
+                                     iters=clipping_iters) * u.adu
+        logger.debug('  Bias Difference ext={:d} (mean, median, stddev) = {:.2f}, {:.2f}, {:.2f}'.format(\
+                     chip, mean, median, stddev))
+        RN = stddev / np.sqrt(1.+1./(nbiases-1))
+        logger.info('Read Noise (ext={:d}) is {:.2f}'.format(chip, RN))
 
-    ax = plt.subplot(312)
-    binsize = 1.0
-    bmin = np.floor(min(bias_diff.g.data.ravel())/binsize)*binsize - binsize/2.
-    bmax = np.ceil(max(bias_diff.g.data.ravel())/binsize)*binsize + binsize/2.
-    bins = np.arange(bmin,bmax,binsize)
-    hist, bins = np.histogram(bias_diff.g.data.ravel(), bins=bins)
-    centers = (bins[:-1] + bins[1:]) / 2
-
-    gaussian = models.Gaussian1D(amplitude=max(hist),\
-                                 mean=bias_diff.g_mean,\
-                                 stddev=RN_g)
-    gaussian_plot = [gaussian(x) for x in centers]
-    plt.bar(centers, hist,\
-            align='center', width=0.7*binsize, log=True, color='g', alpha=0.5,\
-            label='Green CCD Pixel Count Histogram')
-    plt.plot(centers, gaussian_plot, 'k-', alpha=0.8,\
-             label='Gaussian with sigma = {:.2f} ADU'.format(RN_g))
-    plt.plot([bias_diff.g_mean, bias_diff.g_mean], [1, 2*max(hist)],\
-             label='Mean Pixel Value')
-    plt.xlim(np.floor(bias_diff.b_mean-7.*RN_g),\
-             np.ceil(bias_diff.b_mean+7.*RN_g))
-    plt.ylim(10, 2*max(hist))
-    ax.set_xlabel('Counts (ADU)', fontsize=10)
-    ax.set_ylabel('Number of Pixels', fontsize=10)
-    ax.grid()
-    ax.legend(loc='best', fontsize=10)
-    ax.grid()
-
-    ax = plt.subplot(313)
-    binsize = 1.0
-    bmin = np.floor(min(bias_diff.r.data.ravel())/binsize)*binsize - binsize/2.
-    bmax = np.ceil(max(bias_diff.r.data.ravel())/binsize)*binsize + binsize/2.
-    bins = np.arange(bmin,bmax,binsize)
-    hist, bins = np.histogram(bias_diff.r.data.ravel(), bins=bins)
-    centers = (bins[:-1] + bins[1:]) / 2
-
-    gaussian = models.Gaussian1D(amplitude=max(hist),\
-                                 mean=bias_diff.r_mean,\
-                                 stddev=RN_r)
-    gaussian_plot = [gaussian(x) for x in centers]
-    plt.bar(centers, hist,\
-            align='center', width=0.7*binsize, log=True, color='r', alpha=0.5,\
-            label='Red CCD Pixel Count Histogram')
-    plt.plot(centers, gaussian_plot, 'k-', alpha=0.8,\
-             label='Gaussian with sigma = {:.2f} ADU'.format(RN_r))
-    plt.plot([bias_diff.r_mean, bias_diff.r_mean], [1, 2*max(hist)],\
-             label='Mean Pixel Value')
-    plt.xlim(np.floor(bias_diff.b_mean-7.*RN_r),\
-             np.ceil(bias_diff.b_mean+7.*RN_r))
-    plt.ylim(10, 2*max(hist))
-    ax.set_xlabel('Counts (ADU)', fontsize=10)
-    ax.set_ylabel('Number of Pixels', fontsize=10)
-    ax.grid()
-    ax.legend(loc='best', fontsize=10)
-    ax.grid()
+        ##---------------------------------------------------------------------
+        ## Plot Bias Frame Histograms
+        ##---------------------------------------------------------------------
+        logger.debug('  Generating histogram of bias difference image values')
+        bmin = np.floor(min(diff.data.ravel())/binsize)*binsize - binsize/2.
+        bmax = np.ceil(max(diff.data.ravel())/binsize)*binsize + binsize/2.
+        bins = np.arange(bmin,bmax,binsize)
+        hist, bins = np.histogram(diff.data.ravel(), bins=bins)
+        centers = (bins[:-1] + bins[1:]) / 2
+        gaussian = models.Gaussian1D(amplitude=max(hist),\
+                                     mean=mean,\
+                                     stddev=RN)
+        gaussian_plot = [gaussian(x) for x in centers]
+        plt.bar(centers, hist,\
+                align='center', width=0.7*binsize, log=True, color='b', alpha=0.5,\
+                label='Blue CCD Pixel Count Histogram')
+        plt.plot(centers, gaussian_plot, 'b-', alpha=0.8,\
+                 label='Gaussian with sigma = {:.2f}'.format(RN))
+        plt.plot([mean.value, mean.value], [1, 2*max(hist)], label='Mean Pixel Value')
+        plt.xlim(np.floor(mean.value-7.*RN.value), np.ceil(mean.value+7.*RN.value))
+        plt.ylim(1, 2*max(hist))
+        ax.set_xlabel('Counts (ADU)', fontsize=10)
+        ax.set_ylabel('Number of Pixels', fontsize=10)
+        ax.grid()
+        ax.legend(loc='best', fontsize=10)
 
     plotfilename = 'BiasHistogram.png'
-    logger.info('  Saving: {}'.format(plotfilename))
-    plt.savefig(plotfilename, dpi=100)
+    logger.info('Saving: {}'.format(plotfilename))
+    plt.savefig(plotfilename, dpi=72, bbox_inches='tight')
     plt.close()
-    logger.info('  Done.')
+    logger.info('Done.')
+
 
 
     sys.exit(0)
+
+
 
     ##-------------------------------------------------------------------------
     ## Determine Dark Current
