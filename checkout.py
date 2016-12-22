f277a466aaad8506f2b56fb14ab80410a3b9ebdc
Josh Walawender <jmwalawender@gmail.com>
2016-12-21 16:29:31 -1000

    Finish calulation of RN and DC using ccdproc

--- checkout.py
+++ checkout.py
@@ -30,6 +30,18 @@ import matplotlib.pyplot as plt
 
 # from instruments import HIRES
 
+def get_mode(im):
+    '''
+    Return mode of image.  Assumes int values (ADU), so uses binsize of one.
+    '''
+    bmin = np.floor(min(im.data.ravel())) - 1./2.
+    bmax = np.ceil(max(im.data.ravel())) + 1./2.
+    bins = np.arange(bmin,bmax,1)
+    hist, bins = np.histogram(im.data.ravel(), bins=bins)
+    centers = (bins[:-1] + bins[1:]) / 2
+    w = np.argmax(hist)
+    mode = int(centers[w]) * u.adu
+    return mode
 
 
 ##-------------------------------------------------------------------------
@@ -54,9 +66,9 @@ def main():
         action="store_true", dest="noplots",
         default=False, help="Do not make plots.")
     ## add arguments
-#     parser.add_argument("--input",
-#         type=str, dest="input",
-#         help="The input.")
+    parser.add_argument(
+        type=str, dest="input",
+        help="A text file which contains the list of files to use in the analysis.")
     args = parser.parse_args()
 
     plots = not args.noplots
@@ -83,21 +95,27 @@ def main():
 #     LogFileHandler.setFormatter(LogFormat)
 #     logger.addHandler(LogFileHandler)
 
-#     hires = HIRES(logger=logger)
+    ##-------------------------------------------------------------------------
+    ## Sort Input Files
+    ##-------------------------------------------------------------------------
+    assert os.path.exists(args.input)
+    with open(args.input, 'r') as FO:
+        contents = FO.read()
+    files = [line.strip('\n') for line in contents.split('\n') if line != '']
+
+    bias_files = []
+    dark_files = []
+    for file in files:
+        hdr = fits.getheader(file, 0)
+        if hdr.get('OBSTYPE').strip() == 'Bias':
+            bias_files.append(file)
+        elif hdr.get('OBSTYPE').strip() == 'Dark':
+            dark_files.append(file)
 
     ##-------------------------------------------------------------------------
     ## Determine Read Noise
     ##-------------------------------------------------------------------------
-    if args.fake:
-        bias_files = glob('/home/jwalawender/HIRES_Test_Data/*.fits')[0:10]
-    else:
-        pass
-#         nbiases = 3
-#         bias_files = hires.take_bias(n=nbiases)
-
     nbiases = len(bias_files)
-
-    ## Determine Bias Stats for each Chip
     clipping_sigma = 5
     clipping_iters = 1
     if plots:
@@ -106,7 +124,8 @@ def main():
     master_biases = {}
     for chip in [1,2,3]:
         logger.info('Analyzing Chip {:d}'.format(chip))
-        ax = plt.subplot(3,1,chip)
+        if plots:
+            ax = plt.subplot(3,1,chip)
         biases = []
         for bias_file in bias_files:
             logger.debug('  Reading {}'.format(bias_file))
@@ -115,8 +134,9 @@ def main():
                 mean, median, stddev = stats.sigma_clipped_stats(bias.data,
                                              sigma=clipping_sigma,
                                              iters=clipping_iters) * u.adu
-                logger.debug('  Bias ext={:d} (mean, median, stddev) = {:.2f}, {:.2f}, {:.2f}'.format(\
-                             chip, mean, median, stddev))
+                mode = get_mode(bias)
+                logger.debug('  Bias ext={:d} (mean, median, mode, stddev) = {:.1f}, {:.1f}, {:d}, {:.2f}'.format(\
+                             chip, mean, median, mode, stddev))
             else:
                 biases.append(ccdproc.fits_ccddata_reader(bias_file, unit='adu', hdu=chip))
         logger.debug('  Making master bias')
@@ -125,15 +145,17 @@ def main():
         mean, median, stddev = stats.sigma_clipped_stats(master_bias.data,
                                      sigma=clipping_sigma,
                                      iters=clipping_iters) * u.adu
-        logger.debug('  Master Bias ext={:d} (mean, median, stddev) = {:.2f}, {:.2f}, {:.2f}'.format(\
-                     chip, mean, median, stddev))
+        mode = get_mode(master_bias)
+        logger.debug('  Master Bias ext={:d} (mean, median, mode, stddev) = {:.1f}, {:.1f}, {:d}, {:.2f}'.format(\
+                     chip, mean, median, mode, stddev))
 
         diff = bias.subtract(master_bias)
         mean, median, stddev = stats.sigma_clipped_stats(diff.data,
                                      sigma=clipping_sigma,
                                      iters=clipping_iters) * u.adu
-        logger.debug('  Bias Difference ext={:d} (mean, median, stddev) = {:.2f}, {:.2f}, {:.2f}'.format(\
-                     chip, mean, median, stddev))
+        mode = get_mode(diff)
+        logger.debug('  Bias Difference ext={:d} (mean, median, mode, stddev) = {:.1f}, {:.1f}, {:d}, {:.2f}'.format(\
+                     chip, mean, median, mode, stddev))
         RN = stddev / np.sqrt(1.+1./(nbiases-1))
         logger.info('Read Noise (ext={:d}) is {:.2f}'.format(chip, RN))
 
@@ -174,13 +196,7 @@ def main():
     ##-------------------------------------------------------------------------
     ## Determine Dark Current
     ##-------------------------------------------------------------------------
-    if args.fake:
-        dark_files = glob('/home/jwalawender/HIRES_Test_Data/*.fits')[10:]
-    else:
-        pass
-
     ndarks = len(dark_files)
-    ## Determine Dark Stats for each Chip
     clipping_sigma = 5
     clipping_iters = 1
     plt.figure(figsize=(9,15), dpi=72)
@@ -189,7 +205,7 @@ def main():
     dark_table = Table(names=('filename', 'exptime', 'chip', 'mean', 'median', 'stddev'),\
                        dtype=('a64', 'f4', 'i4', 'f4', 'f4', 'f4'))
     logger.info('Analyzing bias subtracted dark frames to measure dark current.')
-    logger.info('  Determining image statistics using sigma clipping algorithm.')
+    logger.info('  Determining image statistics of each dark using sigma clipping algorithm.')
     logger.info('    sigma={:d}, iters={:d}'.format(clipping_sigma, clipping_iters))
 
     for dark_file in dark_files:
@@ -207,74 +223,68 @@ def main():
     line = models.Linear1D(intercept=0, slope=0)
     line.intercept.fixed = True
     fitter = fitting.LinearLSQFitter()
+    dc_fit = {}
     for chip in [1,2,3]:
-        dc_fit = fitter(line, dark_table[dark_table['chip'] == chip]['exptime'],\
+        dc_fit[chip] = fitter(line, dark_table[dark_table['chip'] == chip]['exptime'],\
                               dark_table[dark_table['chip'] == chip]['mean'])
         logger.info('Dark Current (ext={:d}) = {:.2f} ADU/600s'.format(chip,
-                    dc_fit.slope.value*600.))
+                    dc_fit[chip].slope.value*600.))
 
+    if plots:
+        ##-------------------------------------------------------------------------
+        ## Plot Dark Frame Levels
+        ##-------------------------------------------------------------------------
+        logger.info('Generating figure with dark current fits')
+        plt.figure(figsize=(9,15), dpi=72)
+        ax = plt.subplot(111)
+        ax.plot(dark_table[dark_table['chip'] == 1]['exptime'],\
+                dark_table[dark_table['chip'] == 1]['mean'],\
+                'bo',\
+                label='mean count level in ADU (B)',\
+                alpha=1.0)
+        ax.plot([0, 1000],\
+                [dc_fit[1](0), dc_fit[1](1000)],\
+                'b-',\
+                label='dark current (B) = {:.2f} ADU/600s'.format(dc_fit[1].slope.value*600.),\
+                alpha=0.3)
+        ax.plot(dark_table[dark_table['chip'] == 2]['exptime'],\
+                dark_table[dark_table['chip'] == 2]['mean'],\
+                'go',\
+                label='mean count level in ADU (G)',\
+                alpha=1.0)
+        ax.plot([0, 1000],\
+                [dc_fit[2](0), dc_fit[2](1000)],\
+                'g-',\
+                label='dark current (G) = {:.2f} ADU/600s'.format(dc_fit[1].slope.value*600.),\
+                alpha=0.3)
+        ax.plot(dark_table[dark_table['chip'] == 3]['exptime'],\
+                dark_table[dark_table['chip'] == 3]['mean'],\
+                'ro',\
+                label='mean count level in ADU (R)',\
+                alpha=1.0)
+        ax.plot([0, 1000],\
+                [dc_fit[2](0), dc_fit[2](1000)],\
+                'r-',\
+                label='dark current (R) = {:.2f} ADU/600s'.format(dc_fit[3].slope.value*600.),\
+                alpha=0.3)
+        ax.legend(loc='best', fontsize=10)
+        plt.xlim(0, 1.1*max(dark_table['exptime']))
+        plt.ylim(np.floor(min(dark_table['mean'])), np.ceil(max(dark_table['mean'])))
+        ax.set_xlabel('Exposure Time (s)')
+        ax.set_ylabel('Dark Level (ADU)')
+        ax.grid()
+        ax.legend(loc='best', fontsize=10)
+        ax.set_title('Dark Current'\
+                     '\n(Mean computed using sigma clipping: sigma={:d}, '\
+                     'iterations={:d})'.format(\
+                     clipping_sigma, clipping_iters), fontsize=10)
+        ax.grid()
 
-    sys.exit(0)
-
-
-
-
-
-    ##-------------------------------------------------------------------------
-    ## Plot Dark Frame Levels
-    ##-------------------------------------------------------------------------
-    logger.info('Generating figure with dark current fits')
-    plt.figure()
-    ax = plt.subplot(111)
-    ax.plot(dark_table[dark_table['chip'] == 'B']['exptime'],\
-            dark_table[dark_table['chip'] == 'B']['mean'],\
-            'bo',\
-            label='mean count level in ADU (B)',\
-            alpha=1.0)
-    ax.plot([0, 1000],\
-            [dc_fit_B(0), dc_fit_B(1000)],\
-            'b-',\
-            label='dark current (B) = {:.2f} ADU/600s'.format(dc_fit_B.slope.value*600.),\
-            alpha=0.3)
-    ax.plot(dark_table[dark_table['chip'] == 'G']['exptime'],\
-            dark_table[dark_table['chip'] == 'G']['mean'],\
-            'go',\
-            label='mean count level in ADU (G)',\
-            alpha=1.0)
-    ax.plot([0, 1000],\
-            [dc_fit_G(0), dc_fit_G(1000)],\
-            'g-',\
-            label='dark current (G) = {:.2f} ADU/600s'.format(dc_fit_G.slope.value*600.),\
-            alpha=0.3)
-    ax.plot(dark_table[dark_table['chip'] == 'R']['exptime'],\
-            dark_table[dark_table['chip'] == 'R']['mean'],\
-            'ro',\
-            label='mean count level in ADU (R)',\
-            alpha=1.0)
-    ax.plot([0, 1000],\
-            [dc_fit_R(0), dc_fit_R(1000)],\
-            'r-',\
-            label='dark current (R) = {:.2f} ADU/600s'.format(dc_fit_R.slope.value*600.),\
-            alpha=0.3)
-    ax.legend(loc='best', fontsize=10)
-    plt.xlim(0, 1.1*max(dark_table['exptime']))
-    plt.ylim(np.floor(min(dark_table['mean'])), np.ceil(max(dark_table['mean'])))
-    ax.set_xlabel('Exposure Time (s)')
-    ax.set_ylabel('Dark Level (ADU)')
-    ax.grid()
-    ax.legend(loc='best', fontsize=10)
-    ax.set_title('Dark Current'\
-                 '\n(Mean computed using sigma clipping: sigma={:d}, '\
-                 'iterations={:d})'.format(\
-                 clipping_sigma, clipping_iters), fontsize=10)
-    ax.grid()
-
-    plotfilename = 'DarkCurrent.png'
-    logger.info('  Saving: {}'.format(plotfilename))
-    plt.savefig(plotfilename, dpi=100)
-    plt.close()
-    logger.info('  Done.')
-
+        plotfilename = 'DarkCurrent.png'
+        logger.info('  Saving: {}'.format(plotfilename))
+        plt.savefig(plotfilename, dpi=72, bbox_inches='tight')
+        plt.close()
+        logger.info('  Done.')
 
 
 if __name__ == '__main__':
